25c7ea46bf24091cbf46da707b3d45d63cc8547a
yangqianglc <932897867@qq.com>
2023-12-25 18:53:37 +0800

    nothing

---
+++ src/views/layout/menu.js
@@ -0,0 +1,37 @@
+// const Child = props => {
+  //   return (
+  //     <div>
+  //       <h3>jsx -- {props.title}</h3>
+  //       <button onClick={() => props.setTitle(Date.now() + '')}>jsx</button>
+  //     </div>
+  //   )
+  // }
+  
+  // 解构写法
+
+  import menu from './components/menu/demo-playground'
+
+  console.log(menu)
+  
+  const Child = ({ title, setTitle }) => {
+    const clickFun = function () {
+      return setTitle(Date.now() + '')
+    }
+
+
+    return (
+      <div>
+        <h3>jsx -- {title}</h3>
+        <button onClick={clickFun}>jsx</button>
+        {/* <button onClick={() => changeTitle(setTitle)}>jsx</button> */}
+        <el-menu>
+          <el-menu-item index="1">Processing Center</el-menu-item>
+        </el-menu>
+      </div>
+    )
+  }
+
+
+  
+  export default Child
+  
